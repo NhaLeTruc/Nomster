97e2ee9399bd4b296b3c2dfe5128733aaa22871f
Nha Le Truc <lenha777@gmail.com>
2015-11-26 04:12:13 +0000

    User Buttons

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Nomster::Application.configure do
 
-  config.action_mailer.default_url_options = { :host => ' https://nomster-boblee.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://nomster-boblee.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
