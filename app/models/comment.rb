e56dd3c6267a284bd675ebe8bfa117e1735f6c0b
Nha Le Truc <lenha777@gmail.com>
2015-11-28 09:23:35 +0000

    Comment Rating Added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
+
+	RATINGS = {
+		'one star' 		=> '1_star',
+		'two star' 		=> '2_star',
+		'three star' 	=> '3_star',
+		'four star' 	=> '4_star',
+		'five star' 	=> '5_star'
+	}
 end
