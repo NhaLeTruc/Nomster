ff0950476c4aa26e53e302dc263393cf6a74a13e
Nha Le Truc <lenha777@gmail.com>
2015-11-28 07:18:40 +0000

    comment model added

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,11 @@
 class Place < ActiveRecord::Base
 	self.per_page = 5
 	belongs_to :user
+	has_many :comments
+
 	geocoded_by :address
 	after_validation :geocode
+	
 	validates :name, :presence => true, :length => {:minimum => 3}
 	validates :address, :presence => true
 	validates :description, :presence => true
