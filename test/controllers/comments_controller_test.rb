18184b3906829eab79737a39af59ae00087cebfd
Nha Le Truc <lenha777@gmail.com>
2015-12-16 08:11:42 +0000

    Comment create test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -4,4 +4,17 @@ class CommentsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "comment_create" do
+  	user = FactoryGirl.create(:user)
+  	sign_in user
+  	place = FactoryGirl.create(:place)
+
+  	post :create, :place_id => place.id, :comment => {
+  		:message => "BIKE",
+		:rating => "3_star"
+  	}
+
+  	assert_equal 1, user.comments.count
+  	assert_redirected_to place_path(place)
+  end
 end
