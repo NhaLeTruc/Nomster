73cd9d1b8f660e3f2bf463a9d222e5e567b7b7f2
Nha Le Truc <lenha777@gmail.com>
2015-12-16 15:10:34 +0000

    dependent destroy added

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,8 @@
 class Place < ActiveRecord::Base
 	self.per_page = 5
 	belongs_to :user
-	has_many :photos
-	has_many :comments
+	has_many :photos, dependent: :destroy
+	has_many :comments, dependent: :destroy
 
 	geocoded_by :address
 	after_validation :geocode
